41ea5457350ffd1080b675f89906acb92eec1321
William L <frewtypebbles@gmail.com>
2026-03-25 00:03:56 -0700

    Fixed tsup config for packaging wasmoon

--- tsup.config.ts
+++ tsup.config.ts
@@ -15,6 +15,14 @@ const shaderExtensionPlugin: Plugin = {
 };
 
 export default defineConfig({
+  external: [
+    'path',
+    'fs',
+    'child_process',
+    'crypto',
+    'url',
+    'module'
+  ],
   entry: ['src/index.ts'],
   platform: 'browser',
   noExternal: ['wasmoon'],
